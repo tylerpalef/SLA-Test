6ce043c4b119d64930a174f6e09abab8b90b058a
“Tyler <tylerpalef@gmail.com>
2019-01-26 21:38:12 -0500

    finished the second and third examples to show the inclusion and exclusion of how unfinished edits create additional strain on turnaround time as well as an additional OR statement

--- SLA-test.rb
+++ SLA-test.rb
@@ -2,49 +2,34 @@
 def web_edit(web_requested_assigned, web_requested_completed)   # Takes two numbers, adds them, and confirms if the edit was completed within SLA or not.
   turnaround_time = (web_requested_assigned + web_requested_completed)
   if turnaround_time >= 48
-    puts "SLA failure"
+    p "SLA failure"
   else
-    puts "SLA success"
+    p "SLA success"
   end
 end
 
 # web_edit(5, 2)
 
-#
-# # Regarding web edits done with a review, after the initial email is sent to the client
-#
-# def web_edit_with_review(numB, numC, numD, numE, numF)
-#
-#   web_requested_received = 0 # What time we received the initial request
-#   web_requested_assigned = numB # What time it was when the initial email is assigned to someone to edit
-#   web_requested_completed = numC # What time (military time) it was when the edit was done
-#   turnaround_time = (web_requested_completed - web_requested_received) # How long it took for us to respond from the initial email from the client.
-#   if turnaround_time >= 48 # If the time to complete + email the client is greater than SLA
-#     p "failure"
-#   else # If the time to complete the edit + email the client is within SLA
-#     p "success"
-#   end
-#   web_edit_review = numD # How long it took before someone reviewed the edit + email and followed up with the editor
-#   web_edit_update = numE # How long it took before someone made the corrections to the web edit
-#   client_confirm = numF # How long it took before the client saw + responded to our update email
-#   if (web_edit_review - turnaround_time) + (web_edit_update - turnaround_time) >= (client_confirm - turnaround_time)
-#     p "failure"
-#   end
-# end
-#
-# # --------------------
-#
-# # If we process review as a piece of turnaround time
-#
-# def web_edit_with_planned_review(numB, numD, numE)
-#   web_requested_received = 0 # What time we received the initial request
-#   web_requested_assigned = numB # What time it was when the initial email is assigned to someone to edit
-#   web_requested_reviewed_and_edited = (numD - web_requested_assigned) + (numE - numD) # num D represents the review, num E represents the edits.
-#   turnaround_time = (web_requested_reviewed_and_edited - web_requested_received) # How long it took for us to respond from the initial email from the client.
-#
-#   if turnaround_time >= 48 # If the time to complete + email the client is greater than SLA
-#     p "failure"
-#   else # If the time to complete the edit + email the client is within SLA
-#     p "success"
-#   end
-# end
+
+# Regarding web edits done with a review, after the initial email is sent to the client
+def web_edit_with_review(web_requested_assigned, web_requested_completed, web_edit_review, web_edit_update, client_review)
+  turnaround_time = (web_requested_assigned + web_requested_completed)
+  if turnaround_time >= 48 ||  turnaround_time + (web_edit_review + web_edit_update)  >= (turnaround_time + client_review)
+    p "SLA failure" # This is a failure because either we don't finish the edit on time, or we finish with an error the client sees.
+  else
+    p "SLA success"
+  end
+end
+
+# web_edit_with_review(2, 3, 5, 7, 2)
+
+# If we process review as a piece of turnaround time
+
+def web_edit_with_planned_review(web_requested_assigned, web_requested_completed, web_edit_review, web_edit_update)
+  turnaround_time = (web_requested_assigned + web_requested_completed + web_edit_review + web_edit_update)
+  if turnaround_time >= 48 # No longer need to worry about the condition of clients seeing incorrect edits
+    p "failure"
+  else
+    p "success"
+  end
+end
